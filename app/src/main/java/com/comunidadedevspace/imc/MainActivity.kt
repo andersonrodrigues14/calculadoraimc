b55e36881b7b6f19777c907cb3c0712d62160931
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2024-12-06 16:18:44 -0300

    Validando campos vazios

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -4,28 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Recuperando os componentes
-        //criar uma variavel e associal o componente de UI
-        //recuperar o botão da tela
         val edtPeso = findViewById<EditText>(R.id.edt_peso)
         val edtAltura = findViewById<EditText>(R.id.edt_altura)
 
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            val alturaQ1: Float = altura * altura
-            val result: Float = peso / alturaQ1
-            println("Anderson Botão" + result)
+            if (pesoStr.isEmpty() || alturaStr.isEmpty()) {
+                Snackbar.make(edtPeso, "Preencha todos os campos", Snackbar.LENGTH_LONG).show()
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = pesoStr.toFloat()
 
+                val alturaQ1: Float = altura * altura
+                val result: Float = peso / alturaQ1
+            }
         }
     }
 }
