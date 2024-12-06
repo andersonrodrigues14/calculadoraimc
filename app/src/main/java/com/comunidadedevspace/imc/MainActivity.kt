fc3b30deaf99654ddce1c1e33a1c90b3b621c854
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2024-12-06 15:27:55 -0300

    Tela inicial edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,28 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperando os componentes
+        //criar uma variavel e associal o componente de UI
+        //recuperar o botão da tela
+        val edtPeso = findViewById<EditText>(R.id.edt_peso)
+        val edtAltura = findViewById<EditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+            println("Anderson Botão" + peso + altura)
+
+        }
     }
 }
